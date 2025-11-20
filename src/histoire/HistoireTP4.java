8067aa5ac5555e3b0de1c36c322fce77022b2d9d
Drekkou <stormix@yahoo.com>
2025-11-20 14:19:50 +0100

    fin partie 1, creation de la classe humain

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -1,8 +1,16 @@
 package histoire;
 
+import personnages.Humain;
+
 public class HistoireTP4 {
 
 	public static void main(String[] args) {
+		Humain prof = new Humain("Prof", "Kombucha", 54);
 
+		prof.direBonjour();
+		prof.acheter("une boisson", 12);
+		prof.boire();
+		prof.acheter("un jeu", 42);
+		prof.acheter("un kimono", 50);
 	}
 }
--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -5,4 +5,69 @@ public class Humain {
 	private String boissonFav;
 	private int argent;
 
+	public Humain(String nom, String boissonFav, int argent) {
+		super();
+		this.nom = nom;
+		this.boissonFav = boissonFav;
+		this.argent = argent;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getBoissonFav() {
+		return boissonFav;
+	}
+
+	public void setBoissonFav(String boissonFav) {
+		this.boissonFav = boissonFav;
+	}
+
+	public int getArgent() {
+		return argent;
+	}
+
+	public void setArgent(int argent) {
+		this.argent = argent;
+	}
+
+	private String prendreParole() {
+		return "(" + nom + ")" + " - ";
+	}
+
+	protected void parler(String texte) {
+		System.out.println(prendreParole() + texte);
+	}
+
+	public void direBonjour() {
+		parler("Bonjour! Je m'appelle " + this.nom + " et j'aime boire du " + this.boissonFav + ".");
+	}
+
+	public void boire() {
+		parler("Mmmm, un bon verre de " + this.boissonFav + "! GlOUPS !");
+	}
+
+	private void gagnerArgent(int gain) {
+		this.argent += gain;
+	}
+
+	private void perdreArgent(int perte) {
+		this.argent -= perte;
+	}
+
+	public void acheter(String bien, int prix) {
+		if (this.argent >= prix) {
+			parler("J'ai " + this.argent + " sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix
+					+ " sous.");
+			this.perdreArgent(prix);
+		} else {
+			parler("Je n'ai plus que " + this.argent + " sous en poche. Je ne peux même pas m'offrir " + bien + " à "
+					+ prix + " sous.");
+		}
+	}
 }
